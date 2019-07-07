dbde8d6b4115615abad43766503033c42e14a7da
natan.daniel <natan.daniel0@gmail.com>
2019-07-07 16:16:13 +0200

    update handling POST verb

--- ws.rest.cats/src/main/java/cats/CatsServiceImpl.java
+++ ws.rest.cats/src/main/java/cats/CatsServiceImpl.java
@@ -2,13 +2,16 @@ package cats;
 
 import java.beans.XMLDecoder;
 import java.beans.XMLEncoder;
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -16,6 +19,11 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.ws.BindingType;
 import javax.xml.ws.Provider;
@@ -26,6 +34,9 @@ import javax.xml.ws.WebServiceProvider;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.http.HTTPBinding;
 import javax.xml.ws.http.HTTPException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 @WebServiceProvider
 @ServiceMode(value = Service.Mode.MESSAGE)
@@ -35,7 +46,9 @@ public class CatsServiceImpl implements Provider<Source> {
 	@Resource
 	protected WebServiceContext ctx;
 
-	private static final String fileName = "cats.xml";
+	private static final String FILE_NAME = "cats.xml";
+	private static final String PAYLOAD = "payload";
+
 	private byte[] catsByteArray;
 	private List<Cat> catsList;
 	private Map<String, Cat> catsMap;
@@ -43,10 +56,7 @@ public class CatsServiceImpl implements Provider<Source> {
 	public CatsServiceImpl() {
 		catsMap = Collections.synchronizedMap(new HashMap<String, Cat>());
 		readCatsXmlFileToByteArray();
-		deserializeFromXmlBytesToCats();
-
-		System.out.println("Cats : ");
-		catsMap.forEach((k, v) -> System.out.println(v));
+		deserializeFromXmlBytesToCatsObject();
 	}
 
 	@Override
@@ -60,45 +70,23 @@ public class CatsServiceImpl implements Provider<Source> {
 		String httpVerb = (String) msgCtx.get(MessageContext.HTTP_REQUEST_METHOD);
 		httpVerb = httpVerb.trim().toUpperCase();
 
-		System.out.println("HTTP verb : " + httpVerb);
+		//System.out.println("HTTP verb : " + httpVerb);
 
 		if ("GET".equals(httpVerb)) {
 			return doGet(msgCtx);
 		} else if ("POST".equals(httpVerb)) {
-			return doPost(msgCtx);
+			try {
+				return doPost(msgCtx);
+			} catch (Exception e) {
+				e.printStackTrace();
+				throw new HTTPException(500);
+			}
 		} else {
 			throw new HTTPException(405);
 		}
 	}
 
-	private void readCatsXmlFileToByteArray() {
-
-		String currentWorkingDir = System.getProperty("user.dir");
-		String seperator = System.getProperty("file.separator");
-		String pathToFile = currentWorkingDir + seperator + "src" + seperator + "main" + seperator + "resources"
-				+ seperator + seperator + fileName;
-
-		int length = (int) new File(pathToFile).length();
-		catsByteArray = new byte[length];
-
-		try (FileInputStream fileInputStream = new FileInputStream(pathToFile)) {
-			fileInputStream.read(catsByteArray);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	@SuppressWarnings("unchecked")
-	private void deserializeFromXmlBytesToCats() {
-		try (XMLDecoder decoder = new XMLDecoder(new ByteArrayInputStream(catsByteArray))) {
-			catsList = (List<Cat>) decoder.readObject();
-			catsList.forEach(cat -> catsMap.put(cat.getName(), cat));
-		}
-	}
-
-	private Source doGet(MessageContext msgCtx) {
+	private Source doGet(MessageContext msgCtx) throws HTTPException {
 
 		String queryString = (String) msgCtx.get(MessageContext.QUERY_STRING);
 
@@ -111,13 +99,101 @@ public class CatsServiceImpl implements Provider<Source> {
 			if (cat == null) {
 				throw new HTTPException(404);
 			} else {
-				return new StreamSource(serializeFromCatToXmlBytes(cat));
+				return new StreamSource(serializeFromCatObjectToXmlBytes(cat));
 			}
 		}
 	}
-	
-	private Source doPost(MessageContext msgCtx) {
+
+	private Source doPost(MessageContext msgCtx) throws TransformerFactoryConfigurationError, TransformerException,
+			XPathExpressionException, URISyntaxException, IOException {
+
+		@SuppressWarnings({ "rawtypes", "unchecked" })
+		Map<String, List> request = (Map<String, List>) msgCtx.get(MessageContext.HTTP_REQUEST_HEADERS);
+
+		@SuppressWarnings("unchecked")
+		List<String> payload = request.get(PAYLOAD);
+
+		if (payload == null) {
+			throw new HTTPException(400);
+		}
+		
+		//System.out.println("payload : " + payload);
+
+		String xml = "";
+
+		for (String next : payload) {
+			xml += next.trim();
+		}
+
+		//System.out.println("input xml : " + xml);
+
+		ByteArrayInputStream xmlStream = new ByteArrayInputStream(xml.getBytes());
+
+		String name = null;
+		String size = null;
+		String colour = null;
+		String avgWeight = null;
+		String avgAgeExpectancy = null;
+		String coatLength = null;
+		String grooming = null;
+		String lifestyle = null;
+
+		DOMResult dom = new DOMResult();
+		Transformer trans;
+
+		trans = TransformerFactory.newInstance().newTransformer();
+		trans.transform(new StreamSource(xmlStream), dom);
+
+		URI nsUri = new URI("create_cat");
+
+		XPathFactory xpf = XPathFactory.newInstance();
+		XPath xp = xpf.newXPath();
+
+		xp.setNamespaceContext(new NSResolver("", nsUri.toString()));
+
+		name = xp.evaluate("/create_cat/name", dom.getNode());
+		size = xp.evaluate("/create_cat/size", dom.getNode());
+		colour = xp.evaluate("/create_cat/colour", dom.getNode());
+		avgWeight = xp.evaluate("/create_cat/avgWeight", dom.getNode());
+		avgAgeExpectancy = xp.evaluate("/create_cat/avgAgeExpectancy", dom.getNode());
+		coatLength = xp.evaluate("/create_cat/coatLength", dom.getNode());
+		grooming = xp.evaluate("/create_cat/grooming", dom.getNode());
+		lifestyle = xp.evaluate("/create_cat/lifestyle", dom.getNode());
+
+		Cat cat = new Cat(name, size, colour, coatLength, Integer.valueOf(avgAgeExpectancy), Integer.valueOf(avgWeight),
+				grooming, lifestyle);
+
+		catsList.add(cat);
+		catsMap.put(cat.getName(), cat);
+		
+		//catsMap.forEach((k, v) -> System.out.println(v));
+
+		serializeFromCatsObjectToXmlResourceFile(catsList);
+		readCatsXmlFileToByteArray();
+
 		return null;
+	}
+
+	private void readCatsXmlFileToByteArray() {
+
+		int length = (int) new File(getPathToCatsXmlResourceFile()).length();
+		catsByteArray = new byte[length];
+
+		try (FileInputStream fileInputStream = new FileInputStream(getPathToCatsXmlResourceFile())) {
+			fileInputStream.read(catsByteArray);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private void deserializeFromXmlBytesToCatsObject() {
+		try (XMLDecoder decoder = new XMLDecoder(new ByteArrayInputStream(catsByteArray))) {
+			catsList = (List<Cat>) decoder.readObject();
+			catsList.forEach(cat -> catsMap.put(cat.getName(), cat));
+		}
 	}
 
 	private String getValueFromQueryKey(String queryString, String key) {
@@ -131,7 +207,7 @@ public class CatsServiceImpl implements Provider<Source> {
 		}
 	}
 
-	private ByteArrayInputStream serializeFromCatToXmlBytes(Object cat) {
+	private ByteArrayInputStream serializeFromCatObjectToXmlBytes(Object cat) {
 
 		try (ByteArrayOutputStream stream = new ByteArrayOutputStream(); XMLEncoder encoder = new XMLEncoder(stream);) {
 			encoder.writeObject(cat);
@@ -140,5 +216,30 @@ public class CatsServiceImpl implements Provider<Source> {
 		} catch (IOException e) {
 			throw new HTTPException(500);
 		}
+	}
+
+	private void serializeFromCatsObjectToXmlResourceFile(List<Cat> cats) throws IOException {
+
+		String path = getPathToCatsXmlResourceFile();
+
+		BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(path));
+
+		XMLEncoder enc = new XMLEncoder(out);
+		enc.writeObject(cats);
+		enc.flush();
+		enc.close();
+		out.close();
+
+	}
+
+	private String getPathToCatsXmlResourceFile() {
+
+		String currentWorkingDir = System.getProperty("user.dir");
+		String seperator = System.getProperty("file.separator");
+		String path = currentWorkingDir + seperator + "src" + seperator + "main" + seperator + "resources" + seperator
+				+ FILE_NAME;
+		
+		//System.out.println(path);
+		return path;
 	}
 }
--- ws.rest.cats/src/main/java/client/CatsClient.java
+++ ws.rest.cats/src/main/java/client/CatsClient.java
@@ -1,6 +1,9 @@
 package client;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -8,16 +11,24 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class CatsClient {
-	
+
 	private static final String URL = "http://localhost:9082/ws/rest/cats";
+
+	private static final String POST_CAT_1 = "post_cat_1.xml";
 
 	public static void main(String[] args) {
 		getCats();
 		getBritishShorthair();
-	}
-	
-	private static void getCats() {
 		
+		postCat1();
+		
+		getCats();
+		getBritishShorthair();
+		getCat1();
+	}
+
+	private static void getCats() {
+
 		HttpURLConnection connection = getConnection(URL, "GET");
 		try {
 			connection.connect();
@@ -26,9 +37,9 @@ public class CatsClient {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private static void getBritishShorthair() {
-		
+
 		HttpURLConnection connection = getConnection(URL + "?name=British%20Shorthair", "GET");
 		try {
 			connection.connect();
@@ -38,10 +49,34 @@ public class CatsClient {
 		}
 	}
 	
+	private static void getCat1() {
+
+		HttpURLConnection connection = getConnection(URL + "?name=TestCat", "GET");
+		try {
+			connection.connect();
+			print(connection);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void postCat1() {
+
+		HttpURLConnection connection = getConnection(URL, "POST");
+		try {
+			connection.setRequestProperty("payload", readXmlFileToString(getPathToPostCat1XmlFile()));
+			connection.connect();
+			print(connection);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
 	private static HttpURLConnection getConnection(String urlString, String httpVerb) {
-		
+
 		HttpURLConnection connection = null;
-		
+
 		URL url;
 		try {
 			url = new URL(urlString);
@@ -52,28 +87,59 @@ public class CatsClient {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return connection;
 	}
-	
+
 	private static void print(HttpURLConnection connection) {
-		
+
 		BufferedReader bufferedReader = null;
 		String xml = "";
 		String next = null;
-		
+
 		try {
 			bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			
-			while((next = bufferedReader.readLine()) != null) {
+
+			while ((next = bufferedReader.readLine()) != null) {
 				xml += next;
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		System.out.println(xml);
+
+	}
+
+	private static String readXmlFileToString(String path) throws FileNotFoundException, IOException {
+
+		String line = null;
+		StringBuilder sb = new StringBuilder();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(new File(path)))) {
+
+			while ((line = br.readLine()) != null) {
+				sb.append(line.trim() + System.getProperty("line.separator"));
+			}
+		}
 		
+		String xml = sb.toString();
+		xml = xml.replace("\n", "").replace("\r", "");
+
+		//System.out.println("output xml  : " + xml);
+
+		return xml;
+
+	}
+
+	private static String getPathToPostCat1XmlFile() {
+
+		String currentWorkingDir = System.getProperty("user.dir");
+		String seperator = System.getProperty("file.separator");
+		String path = currentWorkingDir + seperator + "src" + seperator + "main" + seperator + "java" + seperator
+				+ "client" + seperator + POST_CAT_1;
+		//System.out.println(path);
+		return path;
 	}
 
 }
